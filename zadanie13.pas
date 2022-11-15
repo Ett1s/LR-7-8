deafee1391fb1bd6ddbd5b9542f68f557d090e23
Kirill <117730839+Ett1s@users.noreply.github.com>
2022-11-15 19:07:36 +0300

    Add files via upload

---
+++ zadanie13.pas
@@ -0,0 +1,33 @@
+﻿var s:string; i:integer; a,b,c:boolean;
+begin
+  
+  read(s);
+  a:=False;
+  b:=False;
+  c:=False;
+  i:=1;
+  
+  while i < Length(s) do
+    begin
+      if s[i]='a' then
+        a:=True;
+      if s[i]='b' then
+        b:=True;
+      if s[i]='c' then
+        c:=True;
+      inc(i);
+    end;
+    
+  if a=True then
+    Writeln('В строке есть только "a".');
+  
+  if b=True then
+    Writeln('В строке есть только "b".');
+  
+  if c=True then
+    Writeln('В строке есть только "c".');
+  
+  if (a=False) and (b=False) and (c=False) then
+    Writeln('Строка не содержит только символы "a", "b" или "с".');
+  
+end.
---
+++ zadanie14.pas
@@ -0,0 +1,18 @@
+﻿var a,b,s:string; x,y,z:integer;
+begin
+  
+a:='world';
+b:='letter';
+readln (s);
+z:=length (s);
+
+while pos ('world',s)<>0 do begin;
+x:= pos ('world',s);
+delete (s,x,5);
+insert (b,s,x);
+y:= pos ('letter',s);
+end;
+
+writeln (s);
+
+end.
